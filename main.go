0172e1aea3f4e0e6279317431d0dd2640b44885e
emiriko <notalvaroaustin@gmail.com>
2024-06-18 12:51:57 +0700

    feat: concurrency using wait group

--- main.go
+++ main.go
@@ -5,60 +5,92 @@ import (
 	"log"
 	"time"
 
+	"sync"
+
 	"github.com/CRAYON-2024/worker/internal"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	var (
+		wg = sync.WaitGroup{}
+	)
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Panic("Error loading .env file")
 	}
 
 	initialTime := time.Now()
+
+	wg.Add(2)
 	// User
-	go fetchAllUser()
+
+	go func() {
+		defer wg.Done()
+		fetchAllUser()
+	}()
 
 	// Post
-	go fetchAllPost()
+	go func() {
+		defer wg.Done()
+		fetchAllPost()
+	}()
+
+	wg.Wait()
 
 	finalTime := time.Now()
 	fmt.Printf("Time taken: %v\n", finalTime.Sub(initialTime))
 }
 
 func fetchAllUser() {
+	var wg sync.WaitGroup
 	// Loop until 10th page ( no validation, as the inferred requirement )
 	for page := 0; page < 10; page++ {
-		userResponse, err := internal.FetchUsers(page)
-
-		if err != nil {
-			log.Fatalf("Error fetching users: %v", err)
-		}
-
-		for _, user := range userResponse.Data {
-			userDetail, err := internal.FetchUserDetail(user.ID)
+		wg.Add(1)
+		go func(pg int) {
+			defer wg.Done()
+			userResponse, err := internal.FetchUsers(pg)
 
 			if err != nil {
-				log.Fatalf("Error fetching user detail: %v", err)
+				log.Fatalf("Error fetching users: %v", err)
 			}
 
-			fmt.Printf("User name %s %s %s %s %s\n", user.Title, user.FirstName, user.LastName, userDetail.Email, userDetail.Gender)
-		}
+			for _, user := range userResponse.Data {
+				userDetail, err := internal.FetchUserDetail(user.ID)
+
+				if err != nil {
+					log.Fatalf("Error fetching user detail: %v", err)
+				}
+
+				fmt.Printf("User name %s %s %s %s %s, countUser \n", user.Title, user.FirstName, user.LastName, userDetail.Email, userDetail.Gender)
+			}
+		}(page)
 	}
+
+	wg.Wait()
 }
 
 func fetchAllPost() {
+	var wg sync.WaitGroup
 	// Loop until 10th page ( no validation, as the inferred requirement )
 
 	for page := 0; page < 10; page++ {
-		postResponse, err := internal.FetchPosts(page)
+		wg.Add(1)
 
-		if err != nil {
-			log.Fatalf("Error fetching posts: %v", err)
-		}
+		go func (pg int) {
+			defer wg.Done()
+			postResponse, err := internal.FetchPosts(pg)
 
-		for _, post := range postResponse.Data {
-			fmt.Printf("Posted by %s %s:\n%s\n\nLikes %d Tags %v\nDate posted %s\n\n", post.Owner.FirstName, post.Owner.LastName, post.Text, post.Likes, post.Tags, post.PublishDate.Format("2006-01-02 15:04:05"))
-		}
+			if err != nil {
+				log.Fatalf("Error fetching posts: %v", err)
+			}
+
+			for _, post := range postResponse.Data {
+				fmt.Printf("Posted by %s %s:\n%s\n\nLikes %d Tags %v\nDate posted %s\n\n", post.Owner.FirstName, post.Owner.LastName, post.Text, post.Likes, post.Tags, post.PublishDate.Format("2006-01-02 15:04:05"))
+			}
+		}(page)
 	}
+
+	wg.Wait()
 }
